87a69a4f710be7b2e49adb6b9a701ce8c635bc96
Ke Luo <camellk24@kes-macbook-pro.local>
2016-03-01 21:38:09 -0700

    - create models for patient, vaccine, and risk factor

---
+++ Models/Vaccine.h
@@ -0,0 +1,15 @@
+//
+//  Vaccine.h
+//  KHHealthCareProj
+//
+//  Created by Ke Luo on 3/1/16.
+//  Copyright © 2016 Ryan Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Vaccine : NSObject
+
+@property (nonatomic, strong) NSString *name;
+
+@end
