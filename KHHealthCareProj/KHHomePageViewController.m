528fa68ec6405c1336737f665d1863df85373242
shavix <davidpr1994@gmail.com>
2016-03-05 13:51:16 -0800

    more changes

---
+++ KHHealthCareProj/KHHomePageViewController.m
@@ -0,0 +1,13 @@
+//
+//  KHHomePageViewController.m
+//  KHHealthCareProj
+//
+//  Created by David Richardson on 3/5/16.
+//  Copyright © 2016 Ryan Lu. All rights reserved.
+//
+
+#import "KHHomePageViewController.h"
+
+@implementation KHHomePageViewController
+
+@end
