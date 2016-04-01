9164fec4310e48cec51e3f1718bcdc9dcd3d2e0c
shavix <davidpr1994@gmail.com>
2016-03-31 22:49:52 -0700

    test

---
+++ KHSignUpViewController.m
@@ -0,0 +1,13 @@
+//
+//  KHSignUpViewController.m
+//  KHHealthCareProj
+//
+//  Created by David Richardson on 3/31/16.
+//  Copyright © 2016 Ryan Lu. All rights reserved.
+//
+
+#import "KHSignUpViewController.h"
+
+@implementation KHSignUpViewController
+
+@end
